40718f0df1d657c83c9b1a52ef82b5c806db0907
masumahemd <rimasum01724@gmail.com>
2023-07-29 15:00:19 +0600

    javascript prectice

---
+++ milestone4/operation.js
@@ -0,0 +1,33 @@
+var eggPrice=500;
+var onion=50;
+eggPrice = eggPrice + 500;
+
+console.log(eggPrice+onion)
+var totalPrice= onion + eggPrice;
+
+var priceDifference=   eggPrice -onion;
+console.log(priceDifference)
+
+
+var priceGet = eggPrice / onion;
+console.log(priceGet)
+
+var multiplication=eggPrice * onion;
+console.log(multiplication);
+console.log('');
+
+
+// string
+var firstName = 'masum';
+var lastName = 'ahmed';
+
+var fullName =firstName +' ' +lastName ;
+console.log(fullName);
+
+var one='30';
+var two=10;
+console.log(one);
+console.log(two);
+
+var convert=parseInt(one)
+console.log(convert);
---
+++ milestone4/variable.js
@@ -0,0 +1,17 @@
+console.log('masum' +'ahmed')
+var price=48;
+console.log('the price of egg is= '+price)
+ 
+var name="the name is masum";
+console.log(name)
+// boolean 
+var serious=true;
+var single=false;
+
+// 1.variable name can not be any keyword
+// var false=86;
+// var return=true;
+
+// 2. 
+var one_two=98;
+console.log(one_two)
