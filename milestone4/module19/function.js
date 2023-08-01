9433a974597b651eafdc50bff122212d2c364ffa
masumahemd <rimasum01724@gmail.com>
2023-08-01 22:31:39 +0600

    function

---
+++ milestone4/module19/function.js
@@ -0,0 +1,16 @@
+// fucntion declearation
+function startFun(){
+    console.log('Get Up  ');
+    console.log('go for the switch ');
+    console.log('click the switch  ');
+
+}
+// halka patla ekta condition maria dilam arki
+var a=1;
+if(a>0){
+    // calling the function
+startFun();
+}
+else{
+    console.log('kissu dekaite ami iccuk na');
+}
---
+++ milestone4/module19/one.js
@@ -0,0 +1,12 @@
+// slicing 
+/**
+ var numbers=[1,2,3,4,5,6,7,8]
+console.log(numbers.slice(1,5));
+ */
+
+
+
+
+
+
+
